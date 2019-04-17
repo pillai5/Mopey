998e2aa22889a53d3df066755915311709b3f625
Sruthi Pillai <pillai5@purdue.edu>
2019-04-16 20:41:41 -0400

    set up google login

--- auth.js
+++ auth.js
@@ -1,21 +1,40 @@
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
 
-module.exports = (passport) => {
-    passport.serializeUser((user, done) => {
+router.use(passport.initialize());
+
+passport.serializeUser((user, done) => {
         done(null, user);
-    });
-    passport.deserializeUser((user, done) => {
+});
+passport.deserializeUser((user, done) => {
         done(null, user);
-    });
-    passport.use(new GoogleStrategy({
+});
+passport.use(new GoogleStrategy({
             clientID: '861693328919-d85bbtdgm0vmu0gn5ubdubm24480qd69.apps.googleusercontent.com',
             clientSecret: '9yQcV5YyVhO9GBN9S1ZNe4G-',
-            callbackURL: 'https://mopey.herokuapp.com/home'
+            callbackURL: 'http://localhost:3000/auth/google/callback'
         },
         (token, refreshToken, profile, done) => {
             return done(null, {
                 profile: profile,
                 token: token
             });
-        }));
-};
+}));
+
+router.get('/', passport.authenticate('google', {
+    scope: ['https://www.googleapis.com/auth/userinfo.profile']
+}));
+
+router.get('/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/'
+    }),
+    (req, res) => {
+        res.redirect('/home')
+        //console.log('we out here')
+    }
+);
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const app = express();
-const parser = require('body-parser');
-//const morgan = require('morgan');
 const users = require('./routers/user')
 const login = require('./routers/login')
 const home = require('./routers/home');
@@ -15,12 +13,13 @@ mongoose.connect('mongodb://localhost/mopey')
 
 
 app.use(express.json());
-app.use(express.urlencoded(extended = true));
-app.use('/:userID', users);
-app.use('/login', login);
+app.use(express.urlencoded({extended: true}));
+//app.use('/:userID', users);
+//app.use('/login', login);
 //app.use('/', home);
-auth(passport);
-app.use(passport.initialize());
+app.use('/auth/google', auth);
+//auth(passport);
+
 
 app.get('/', (req,res)=> {
     res.json({
@@ -28,16 +27,7 @@ app.get('/', (req,res)=> {
     });
 })
 
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['https://www.googleapis.com/auth/userinfo.profile']
-}));
 
-app.get('/auth/google/callback',
-    passport.authenticate('google', {
-        failureRedirect: '/'
-    }),
-    (req, res) => {}
-);
 //const schema = new mongoose.Scheme({})
 //
 const port = process.env.PORT ||  3000;
