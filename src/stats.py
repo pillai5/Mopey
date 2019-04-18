8e5ca6cfea5c68215ae4107fdc8e4a6ba33367de
jeanellet <jeanellet@users.noreply.github.com>
2019-04-18 01:52:47 -0400

    extract csv info

--- src/stats.py
+++ src/stats.py
@@ -1,11 +1,27 @@
 
 import csv
+import numpy as np
 import pygal
 #from ibm_watson import ToneAnalyzerV3
 
 if __name__ == '__main__':
+
+    data=[]
+
     filepath="../data.csv"
     with open(filepath,encoding="utf8") as csvfile:
         reader=csv.reader(csvfile,delimiter=",")
+        i=0
         for row in reader:
-            print(row)
+            data.append(row)
+    data=np.array(data)
+    print(data)
+
+    chart=pygal.Bar()
+    chart.title="Moods of Users"
+    users=[]
+    for i in data:
+        users.append(i[0])
+    users=users[1:]
+    print(users)
+    chart.x_labels=
