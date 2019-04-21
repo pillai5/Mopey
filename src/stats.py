b40d1424c46f1d91c03ef38220863148b543980c
jeanellet <jeanellet@users.noreply.github.com>
2019-04-21 18:43:06 -0400

    ibm tone analysis setup

--- src/stats.py
+++ src/stats.py
@@ -1,27 +1,22 @@
 
-import csv
 import numpy as np
 import pygal
-#from ibm_watson import ToneAnalyzerV3
+import json
+import pymongo
+from pymongo import MongoClient
+from ibm_watson import ToneAnalyzerV3
 
 if __name__ == '__main__':
 
-    data=[]
+    client=MongoClient()
 
-    filepath="../data.csv"
-    with open(filepath,encoding="utf8") as csvfile:
-        reader=csv.reader(csvfile,delimiter=",")
-        i=0
-        for row in reader:
-            data.append(row)
-    data=np.array(data)
-    print(data)
+    analyzer=ToneAnalyzerV3(version='2017-09-21',iam_apikey='0jo9KTyE-iEs9tCMlGjxsyxCXe1AIWHJVsLaGEc68wNj',url='https://gateway.watsonplatform.net/tone-analyzer/api')
 
-    chart=pygal.Bar()
-    chart.title="Moods of Users"
-    users=[]
-    for i in data:
-        users.append(i[0])
-    users=users[1:]
-    print(users)
-    chart.x_labels=
+    text="Im really self assured."
+
+    analysis=analyzer.tone({'text':text},content_type='application/json').get_result()
+    for i in range(len(analysis['document_tone']['tones'])):
+        print("TONE "+str(i)+": "+str(analysis['document_tone']['tones'][i]['tone_name']))
+    #print(type(analysis['document_tone']['tones']))
+    #print(json.dumps(analysis,indent=2))
+    #print(analysis['document_tone']['tones'])
