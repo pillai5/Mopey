641fadfb2af646ad8427c18c5127035858245d8a
niharika01 <npujar@purdue.edu>
2019-04-22 03:12:10 -0400

    minor changes

--- src/public/generic.js
+++ src/public/generic.js
@@ -3,7 +3,9 @@ note:''
  }]
 document.querySelector('#search-text').addEventListener('input',function(e){
    entry.note= e.target.value;
-   const noteEl=document.createElement('p')
-   noteEl.textContent=entry.note
-   document.querySelector('#main').appendChild(noteEl)
+   
 })
+document.querySelector('#add-entry').addEventListener(onclick)=function(e){
+const noteEl=document.createElement('p')
+   noteEl.textContent=entry.note
+   document.querySelector('#main').appendChild(noteEl)}
