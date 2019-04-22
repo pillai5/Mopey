46f73533cd3bd51c4ef591a2bc8760b98c73423c
niharika01 <npujar@purdue.edu>
2019-04-22 15:54:57 -0400

    dynamic paragraphs

--- src/public/generic.js
+++ src/public/generic.js
@@ -1,11 +1,38 @@
- const entry=[{
-note:''
- }]
-document.querySelector('#search-text').addEventListener('input',function(e){
-   entry.note= e.target.value;
+var newtask = document.getElementById( 'newEntry' );
+var btn = document.getElementById( 'add-entry' );
+var nTaskPlace = document.getElementById( 'main' );
+
+var AddEntry = function() {
    
-})
-document.querySelector('#add-entry').addEventListener(onclick)=function(e){
-const noteEl=document.createElement('p')
-   noteEl.textContent=entry.note
-   document.querySelector('#main').appendChild(noteEl)}
+    var paragraph = document.createElement( 'p' );
+    paragraph.innerHTML = newtask.value;
+    var editInput = document.createElement( 'input' );
+    editInput.style.display= 'none';
+    editInput.value = newtask.value;
+    var btnEdit = document.createElement( 'button' );
+    btnEdit.innerHTML = 'Edit';
+    var btnSave = document.createElement( 'button' );
+    btnSave.innerHTML = 'Save';
+    btnSave.style.display= 'none';
+    btnEdit.addEventListener( 'click', function(){
+        editInput.style.display = 'inline-block';
+        paragraph.style.display = 'none';
+        btnSave.style.display = 'inline-block';
+        btnEdit.style.display = 'none';
+    });
+    btnSave.addEventListener( 'click',function(){
+        editInput.style.display = 'none';
+        paragraph.style.display = 'inline-block';
+        btnSave.style.display = 'none';
+        btnEdit.style.display = 'inline-block';
+        paragraph.innerHTML = editInput.value;    
+    }); 
+    var containerDiv = document.createElement( 'div' );
+    containerDiv.appendChild( paragraph );
+    containerDiv.appendChild( editInput );
+    containerDiv.appendChild( btnEdit );
+    containerDiv.appendChild( btnSave );
+    newtask.value = '';
+    nTaskPlace.appendChild( containerDiv, nTaskPlace.firstChild );
+}
+btn.addEventListener( 'click', AddEntry );
