d544030383e7cbeff11cc7daceeff855f049bab6
niharika01 <npujar@purdue.edu>
2019-04-25 14:18:09 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -133,9 +133,9 @@ app.post('/addentry', (req, res) => {
             entry: req.body.entry
         });
 
-        // newJournal.save((err, journal) => {
-        //     if (err) return console.error(err);
-        // });
+        newJournal.save((err, journal) => {
+           if (err) return console.error(err);
+        });
     }
 })
 const port = process.env.PORT || 3000;
